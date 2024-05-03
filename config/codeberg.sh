323ba02741962b0932d4524dcbc9af9e2ca398e9
xtex <xtexchooser@duck.com>
2024-05-03 17:06:09 +0800

    Fix codeberg config

--- config/codeberg.sh
+++ config/codeberg.sh
@@ -2,6 +2,6 @@
 
 SYNCER_TYPE=forgejo
 SYNCER_DEST=https://github.com/xtexChooser/git-mirror.git
-SYNCER_DEST_PREFIX=refs/heads/mirror/exogit/
+SYNCER_DEST_PREFIX=refs/heads/mirror/codeberg/
 FORGEJO_BASE_URL=https://codeberg.org
 FORGEJO_USERNAME=xtex
