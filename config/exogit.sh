818b4d3f077cf5f623fe25e26eb62f33fba4ee35
xtex <xtexchooser@duck.com>
2024-05-03 17:48:26 +0800

    Update configs

--- config/exogit.sh
+++ config/exogit.sh
@@ -1,7 +1,7 @@
 # shellcheck disable=SC2034
 
 SYNCER_TYPE=forgejo
-SYNCER_DEST=https://xtexChooser:"$GH_TOKEN"@github.com/xtexChooser/git-mirror.git
-SYNCER_DEST_PREFIX=refs/heads/mirror/exogit/xtex/
+SYNCER_DEST=https://xtexChooser:"$GH_TOKEN"@github.com/xtexChooser/central.git
+SYNCER_DEST_PREFIX=refs/heads/exogit/xtex/
 FORGEJO_BASE_URL=https://git.exozy.me
 FORGEJO_USERNAME=xtex
