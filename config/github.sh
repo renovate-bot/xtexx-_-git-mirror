cfbf01e9d90d92042e7ef21d6b9784740372e821
xtex <xtexchooser@duck.com>
2024-05-03 21:09:43 +0800

    fix gh

--- config/github.sh
+++ config/github.sh
@@ -5,4 +5,4 @@ SYNCER_DEST=https://xtexChooser:"$GH_TOKEN"@github.com/xtexChooser/central.git
 SYNCER_DEST_PREFIX=refs/heads/github/xtex/
 SYNCER_DEST_TAGS_PREFIX=refs/tags/github/xtex/
 GITHUB_BASE_URL=https://api.github.com
-GITHUB_USERNAME=xtexChooser
+GITHUB_USERNAME=users/xtexChooser
