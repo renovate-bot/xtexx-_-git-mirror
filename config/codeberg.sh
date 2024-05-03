569b2bb5dd94a2c75ac7b070e71971141a2b70fb
xtex <xtexchooser@duck.com>
2024-05-03 17:29:08 +0800

    Use PAT to push

--- config/codeberg.sh
+++ config/codeberg.sh
@@ -1,7 +1,7 @@
 # shellcheck disable=SC2034
 
 SYNCER_TYPE=forgejo
-SYNCER_DEST=https://github.com/xtexChooser/git-mirror.git
+SYNCER_DEST=https://xtexChooser:"$GH_TOKEN"@github.com/xtexChooser/git-mirror.git
 SYNCER_DEST_PREFIX=refs/heads/mirror/codeberg/xtex/
 FORGEJO_BASE_URL=https://codeberg.org
 FORGEJO_USERNAME=xtex
