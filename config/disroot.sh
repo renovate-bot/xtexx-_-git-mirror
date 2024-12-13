af06053507a0f19194d0a1d9a2b4cef2b4971e66
xtex <xtexchooser@duck.com>
2024-12-13 22:42:54 +0800

    Add disroot and pub.solar mirror

---
+++ config/disroot.sh
@@ -0,0 +1,8 @@
+# shellcheck disable=SC2034
+
+SYNCER_TYPE=forgejo
+SYNCER_DEST=https://xtexChooser:"$GH_TOKEN"@github.com/xtexChooser/central.git
+SYNCER_DEST_PREFIX=refs/heads/disroot/xtex/
+SYNCER_DEST_TAGS_PREFIX=refs/tags/disroot/xtex/
+FORGEJO_BASE_URL=https://git.disroot.org
+FORGEJO_USERNAME=xtex
